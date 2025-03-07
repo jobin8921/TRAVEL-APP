2a0b280bce7df55588ea720c9be22000cf5761af
Eibel207 <eibelks60@gmail.com>
2025-03-07 23:27:33 +0530

    setup the registration

--- home/views.py
+++ home/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password
+from home.models import Customer
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        phone_number = request.POST['phone_number']
+        password = request.POST['password']
+        gender = request.POST['gender']
+        city = request.POST['city']
+        state = request.POST['state']
+        country = request.POST['country']
+
+        # Save user
+        customer = Customer(
+            username=username,
+            email=email,
+            phone_number=phone_number,
+            password=make_password(password),  # Hashing password
+            gender=gender,
+            city=city,
+            state=state,
+            country=country
+        )
+        customer.save()
+        return redirect('register')  # Redirect after successful registration
+
+    return render(request, 'registration.html')
+
