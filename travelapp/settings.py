27500f57e1112bd3b0ef3b7395e169fb2820d802
Eibel207 <eibelks60@gmail.com>
2025-03-16 23:34:32 +0530

    updated-templates

--- travelapp/settings.py
+++ travelapp/settings.py
@@ -124,14 +124,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+import os
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'  # URL to access static files
+
+# Directory where static files are collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional directories to look for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
